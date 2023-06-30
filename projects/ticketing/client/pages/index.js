ae6586944272e47adcf72ec2ca151ebf618dc4d2
Marco Martínez <marco.martinez@unosquare.com>
2023-06-29 20:17:27 -0600

    Added ssr API client & index current user request

--- projects/ticketing/client/pages/index.js
+++ projects/ticketing/client/pages/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import buildClient from "../api/buildClient";
 
-const Landing = () => {
-  return <div>Landing</div>;
+export const getServerSideProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return { props: { currentUser: data.currentUser } };
+};
+
+const Landing = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 export default Landing;
