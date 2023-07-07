1124fda330521b1a25a67cef58a223b0227cb895
Marco Martínez <marco.martinez@unosquare.com>
2023-07-07 17:27:09 -0600

    Added common library for reusable code across services

--- projects/ticketing/auth/src/routes/current-user.ts
+++ projects/ticketing/auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from "express";
-
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@mmticketstest/common";
 
 const router = express.Router();
 
--- projects/ticketing/auth/src/routes/signin.ts
+++ projects/ticketing/auth/src/routes/signin.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
+import { validateRequest, BadRequestError } from "@mmticketstest/common";
 
-import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 import { Password } from "../utils/password";
 
 const router = express.Router();
